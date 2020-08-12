797aa438ce2e57df448fb2e8a4d85c3bf801d99f
_Pear <63015754+CobyPear@users.noreply.github.com>
2020-08-12 09:49:55 -0500

    Updates

---
+++ precache-manifest.b69e2dc821901c88da1d8a44fe6f109f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7823933ada9f980350e01be57d33d05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa568b53bae0841497f3",
+    "url": "/static/js/2.ece519a5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ece519a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60bd0fe96c926134e411",
+    "url": "/static/js/main.34b7940c.chunk.js"
+  },
+  {
+    "revision": "c74b6dcb725f329d7bfe",
+    "url": "/static/js/runtime-main.403e0656.js"
+  }
+]);
